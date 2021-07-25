b21d7b1258d1560d06953b498a6570d27dda7584
Ahmed Galadi <galadiahmed99@gmail.com>
2021-07-25 22:05:11 +0100

    making category dropdown

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -7,6 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
+
+    men = {type: "Men", class: "men-title", content: ["T-shitrs", "Sweatshirts", "Shorts&Pants", "Coats&Jackets", "Hats"] };
+    wemen = {type: "Women", class: "wemen-title", content: ["T-shirt" , "Blouse&Shirts", "Tanks&Camis", "Outerwear&Coats"] };
+    kids = {type: "Kids", class: "kids-title", content: ["Baby boy", "Baby girl"] };
+
+    listType: string = '';
+    listClass: string = '';
+    listContent: string[] = [];
+    isHidden = true;
+
   searchInput = '';
   searchActive = false;
 
@@ -14,6 +24,38 @@ export class NavbarComponent implements OnInit {
 
   ngOnInit(): void {
   }
+
+
+  onMouseOut() {
+    this.listType = '';
+    this.listClass= '';
+    this.listContent= [];
+    this.isHidden= true;
+  }
+
+  mouseOnMan() {
+    this.listType = this.men.type;
+    this.listClass= this.men.class;
+    this.listContent= this.men.content;
+    this.isHidden= false;
+  }
+  mouseOnWoman() {
+    this.listType = this.wemen.type;
+    this.listClass= this.wemen.class;
+    this.listContent= this.wemen.content;
+    this.isHidden= false;
+  }
+
+  mouseOnKids(){
+    this.listType = this.kids.type;
+    this.listClass= this.kids.class;
+    this.listContent= this.kids.content;
+    this.isHidden= false;
+  }
+
+  mouseOnDecorations(){}
+
+  mouseOnBeauty(){}
 
 
   onSearch() {
